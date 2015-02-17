276661539894d9e106eb44267e85f5212d5fc1fb
Rachel Glomski <rrglomsk@oakland.edu>
2015-02-16 22:22:18 -0500

    Add more animations

---
+++ SpaceCat/Assets/Scripts/Select Stage/PlanetSelect.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetSelect : MonoBehaviour {
+
+	private bool selected = false;
+	private string selectedPlanet;
+	private Animator animator;
+
+	
+	// Use this for initialization
+	void Start () {
+		selectedPlanet = this.name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.platform == RuntimePlatform.Android) {
+			if (Input.touchCount > 0) {
+				if (Input.GetTouch (0).phase == TouchPhase.Began) {
+					CheckTouch (Input.GetTouch (0).position, "began");
+				}  else if (Input.GetTouch (0).phase == TouchPhase.Ended) {
+					CheckTouch (Input.GetTouch (0).position, "ended");
+				}
+			}
+		}
+	}
+	
+	void CheckTouch(Vector3 pos, string phase) {
+		Vector3 wp = Camera.main.ScreenToWorldPoint(pos);
+		Vector2 touchPos = new Vector2(wp.x, wp.y);
+		Collider2D hit = Physics2D.OverlapPoint(touchPos);
+
+		if (hit.gameObject.name == "Planet 1" && hit && phase == "began") {
+
+		}
+
+		if (hit.gameObject.name == "Planet 1" && hit && phase == "ended") {
+			Application.LoadLevel ("Planet 1");
+		}
+	}
+}
---
+++ SpaceCat/Assets/Scripts/Stage Controls/GravityControl.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityControl : MonoBehaviour {
+
+	private GameObject spaceCat;
+	public Vector3 jumpMovement = new Vector3();
+	private Animator animator;
+	
+	
+	// Use this for initialization
+	void Start () {
+		spaceCat = GameObject.Find("cat");
+		animator = GameObject.Find ("cat").GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.platform == RuntimePlatform.Android) {
+			if (Input.touchCount == 1) {
+				if (Input.GetTouch (0).phase == TouchPhase.Began) {
+					CheckTouch (Input.GetTouch (0).position, "began");
+				} else if (Input.GetTouch (0).phase == TouchPhase.Ended) {
+					CheckTouch (Input.GetTouch (0).position, "ended");
+				}
+			} else if (Input.touchCount > 1) {
+				if (Input.GetTouch (1).phase == TouchPhase.Began) {
+					CheckTouch (Input.GetTouch (1).position, "began");
+				} else if (Input.GetTouch (1).phase == TouchPhase.Ended) {
+					CheckTouch (Input.GetTouch (1).position, "ended");
+				}
+			}
+		}
+	}
+	
+	void CheckTouch(Vector3 pos, string phase) {
+		Vector3 wp = Camera.main.ScreenToWorldPoint(pos);
+		Vector2 touchPos = new Vector2(wp.x, wp.y);
+		Collider2D hit = Physics2D.OverlapPoint(touchPos);
+		
+		if (hit.gameObject.name == "GravityUp" && hit && phase == "began")
+		{
+			spaceCat.rigidbody2D.gravityScale = -0.09f;
+			animator.SetBool("Floating", true);
+
+			
+		}
+		
+		if (hit.gameObject.name == "GravityDown" && hit && phase == "began")
+		{
+			spaceCat.rigidbody2D.gravityScale = 0.15f;
+			animator.SetBool("Floating", false);
+		}
+	}
+
+
+}
